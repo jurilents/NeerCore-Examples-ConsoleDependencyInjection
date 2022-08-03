cf5fff2faf4337fc36e8be40e8a01f984484791c
jurilents <keeeneeek@gmail.com>
2022-08-03 23:52:54 +0300

    Temp fix

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ using NeerCore.DependencyInjection.Extensions;
 using NeerCore.Examples.ConsoleDependencyInjection.Services;
 
 var services = new ServiceCollection();
-services.AddAllServices(options =>
+// TODO: Use 'AddAllServices' after NeerCore v1.5.0
+services.AddServicesFromCurrentAssembly(options =>
 {
     options.DefaultLifetime = ServiceLifetime.Transient;
 });
